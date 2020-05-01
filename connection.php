dbe926dbcb694ae904b3d4cb604c063ae65dc74a
wynrev <wayanrev22@gmail.com>
2020-05-02 01:50:41 +0700

    mengubah isi file connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,11 @@
 <?php
 
-$host = ("localhost");
-$username = ("root");
-$password = ("");
-$database = ("infoterkni");
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "infoterkni";
+
+$connection = mysqli_connect($host, $username, $password, $database);
 
 if (mysqli_connect_errno()) {
 	echo "Koneksi database gagal : " . mysqli_connect_error();
