a6c977dab98a26ebe83723ba40d930c4902fb268
Wayan Revyandi <64642807+wynrev@users.noreply.github.com>
2020-05-02 19:14:38 +0700

    Mengubah isi dari file Database_schema.php (#7)

--- database_schema.php
+++ database_schema.php
@@ -9,9 +9,37 @@ $sql = "CREATE TABLE IF NOT EXISTS user (
   password VARCHAR(255) NOT NULL)";
 
 if (mysqli_query($connection, $sql)) {
-  echo "Table User created successfully";
+	echo "Table User created successfully";
 } else {
-  echo "Error creating table: " . mysqli_error($connection);
+	echo "Error creating table: " . mysqli_error($connection);
+}
+
+$sql = "CREATE TABLE IF NOT EXISTS news_category (
+  id INT(255) AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL)";
+
+if (mysqli_query($connection, $sql)) {
+	echo "Table News Category created successfully";
+} else {
+	echo "Error creating table: " . mysqli_error($connection);
+}
+
+$sql = "CREATE TABLE IF NOT EXISTS news (
+  id INT(255) AUTO_INCREMENT PRIMARY KEY,
+  id_user INT(255) ,
+  category_id INT(255),
+  title VARCHAR(255) UNIQUE NOT NULL,
+  slug VARCHAR(255) UNIQUE NOT NULL,
+  body TEXT,
+  created DATETIME,
+  FOREIGN KEY (id_user) REFERENCES user (id),
+  FOREIGN KEY (category_id) REFERENCES news_category (id)
+  )";
+
+if (mysqli_query($connection, $sql)) {
+	echo "Table News created successfully";
+} else {
+	echo "Error creating table: " . mysqli_error($connection);
 }
 
 $sql = "CREATE TABLE IF NOT EXISTS news_category (
